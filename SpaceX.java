54715750b345b23ef8bd9034701ecdd0a24fd5fe
izayaaaaaaa <izaya.orihara0011@gmail.com>
2022-12-02 20:55:08 +0800

    final program finished by lunch

--- SpaceX.java
+++ SpaceX.java
@@ -1,55 +1,53 @@
-// what is plaf? possible explanation for grey text area default? (OPTIONAL)
 import javax.swing.*;
 import javax.swing.border.LineBorder;
-// import javax.swing.plaf.DimensionUIResource;
-
+import javax.swing.plaf.ComboBoxUI;
+import javax.swing.plaf.basic.BasicArrowButton;
+import javax.swing.plaf.basic.BasicComboBoxUI;
 import java.awt.*;
 
 public class SpaceX {
-  private static void addComponentstoPane(Container pane) {
-    // top level frame is a 2-row-grid with two panels 
+  private static void addComponentstoPane(Container pane) { // top level frame is a 2-row-grid with two panels 
+    
     pane.add(createTopPanel());
     pane.add(createBottomPanel());
-
-    // change font of every text
-    Font font = new Font("Arial Nova", Font.BOLD, 15);
-    changeFont(pane, font);
+    
+    Font font = new Font("Arial Nova", Font.BOLD, 15); 
+    changeFont(pane, font); // change font of every text
   }
 
-  private static JPanel createTopPanel() {
-    // top panel is a 5-row-grid with flow-layout for the first four rows and a fifth grid-layout
-
+  private static JPanel createTopPanel() { // top panel is a 5-row-grid with flow-layout for the first four rows and a fifth grid-layout
+    
     JPanel topPanel = new JPanel(new GridLayout(5, 1));
     JPanel row1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    JPanel findSatPanel = new JPanel();
     JPanel row2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
     JPanel row3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
     JPanel row4 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-    JPanel row5 = new JPanel(new GridLayout(1, 3, 5, 0));
+    JPanel row5 = new JPanel(new GridLayout(1, 3, -5, 0));
     JPanel row5_save = new JPanel();
     JPanel row5_update = new JPanel();
     JPanel row5_delete = new JPanel();
-    // place findSat inside a button to add empty border or "gaps"
-    JPanel findSatPanel = new JPanel();
 
     JLabel satId = new JLabel("Satellite ID");
     JTextField satIdField = new JTextField(20);
     JButton findSat = new JButton("Find Satellite");
     JLabel satName = new JLabel("Satellite Name");
-    JTextField satNameField = new JTextField(45);
+    JTextField satNameField = new JTextField(49);
     JLabel longi = new JLabel("Longitude");
     JTextField longiField = new JTextField(20);
     JLabel lati = new JLabel("Latitude");
-    JTextField latiField = new JTextField(15);
+    JTextField latiField = new JTextField(16);
     JLabel elev = new JLabel("Elevation"); 
     JTextField elevField = new JTextField(20); 
     JLabel healthStat = new JLabel("Health Status");
-    JTextField healthStatField = new JTextField(15);
+    JComboBox healthStatField = new JComboBox();
     JButton save = new JButton("Save"); 
     JButton update = new JButton("Update");
     JButton delete = new JButton("Delete");
 
     // components sizes 
-    findSat.setPreferredSize(new Dimension (280,23));
+    findSat.setPreferredSize(new Dimension (330,23));
+    healthStatField.setPreferredSize(new Dimension(200, 23));
     save.setPreferredSize(new Dimension (225,23));
     update.setPreferredSize(new Dimension (225,23));
     delete.setPreferredSize(new Dimension (225,23));
@@ -61,23 +59,27 @@ public class SpaceX {
     satName.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 15));
     findSatPanel.setBorder(BorderFactory.createEmptyBorder(0, 9, 0, 0));
     longi.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 45));
+    lati.setBorder(BorderFactory.createEmptyBorder(0, 14, 0,66));
     elev.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 50));
+    healthStat.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 39));
 
     // border line 
-    findSat.setBorder(new LineBorder(new Color(202,202,202), 2));
-    save.setBorder(new LineBorder(new Color(202,202,202), 2));
-    update.setBorder(new LineBorder(new Color(202,202,202), 2));
-    delete.setBorder(new LineBorder(new Color(202,202,202), 2));
-
-    
     satIdField.setBorder(BorderFactory.createLineBorder(new Color(191,191,191), 2));
     satNameField.setBorder(BorderFactory.createLineBorder(new Color(191,191,191), 2));
     longiField.setBorder(BorderFactory.createLineBorder(new Color(191,191,191), 2));
     latiField.setBorder(BorderFactory.createLineBorder(new Color(191,191,191), 2));
     elevField.setBorder(BorderFactory.createLineBorder(new Color(191,191,191), 2));
-    healthStatField.setBorder(BorderFactory.createLineBorder(new Color(191,191,191), 2));
+    
+    findSat.setBorder(new LineBorder(new Color(202,202,202), 2));
+    healthStatField.setBorder(new LineBorder(new Color(191,191,191), 2));
+    save.setBorder(new LineBorder(new Color(202,202,202), 2));
+    update.setBorder(new LineBorder(new Color(202,202,202), 2));
+    delete.setBorder(new LineBorder(new Color(202,202,202), 2));
 
     // component background
+    healthStatField.setUI(ColorArrowUI.createUI(healthStatField));
+    healthStatField.setBackground(Color.white);
+
     findSat.setBackground(new Color(223,223,223));
     save.setBackground(new Color(223,223,223));
     update.setBackground(new Color(223,223,223));
@@ -90,7 +92,6 @@ public class SpaceX {
     lati.setForeground(new Color(97,160,255));
     elev.setForeground(new Color(97,160,255));
     healthStat.setForeground(new Color(97,160,255)); 
-    // turn into a combo box ^^^
 
     row1.add(satId); 
     row1.add(satIdField);
@@ -124,9 +125,8 @@ public class SpaceX {
 
   private static JPanel createBottomPanel() {
     // bottom border-layout panel with the center text area and a south clearall button
-    // border-layout is best used when attempting maximize space coupled with a south component according to oracle java docs
 
-    JPanel bottomPanel = new JPanel(new BorderLayout());
+    JPanel bottomPanel = new JPanel(new BorderLayout(0, 7));
     JPanel bottomButtonPanel = new JPanel();
     // add gap between center and south
 
@@ -162,15 +162,14 @@ public class SpaceX {
         changeFont(child, font);
       }
     }
-    // automate changing font color?
   }
-   
-  private static void createAndShowGUI() {
+
+  private void createAndShowGUI() {
     JFrame mainFrame = new JFrame("SpaceX Starlink Project");
     mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     ImageIcon icon = new ImageIcon("./SpaceX_Icon.png");
     mainFrame.setIconImage(icon.getImage());
-    mainFrame.setLayout(new GridLayout(2, 1, 0, 15));
+    mainFrame.setLayout(new GridLayout(2, 1, 0, 10));
   
     addComponentstoPane(mainFrame.getContentPane()); 
 
@@ -181,6 +180,21 @@ public class SpaceX {
   }
   
   public static void main(String[] args) {
-    createAndShowGUI();
+    SpaceX main = new SpaceX();
+    main.createAndShowGUI();
+  }
+}
+
+class ColorArrowUI extends BasicComboBoxUI {
+
+  public static ComboBoxUI createUI(JComponent c) {
+      return new ColorArrowUI();
+  }
+
+  @Override protected JButton createArrowButton() {
+      return new BasicArrowButton(
+          BasicArrowButton.SOUTH,
+          Color.white, Color.white,
+          Color.gray, Color.white);
   }
 }
